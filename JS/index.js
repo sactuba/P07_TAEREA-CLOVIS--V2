b8a5bf197b40d5c6f55757e1140f1ce36c4eb75e
sactuba <taloamn85@gmail.com>
2022-03-15 19:40:04 +0100

    mise a jour de l'interface par rapport a la maquette sur figma

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+function recipesCards() {
+  let container = document.getElementById("recipes_container");
+  for (let i = 0; i < recipes.length; i++) {
+    const element = recipes[i];
+    console.log(element);
+  }
+}
+
+recipesCards();
