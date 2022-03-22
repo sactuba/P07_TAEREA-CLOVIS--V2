18be53e832d4b919661fd3169456eedaf84f74f6
sactuba <taloamn85@gmail.com>
2022-03-22 13:54:48 +0100

    misae a jour

--- JS/index.js
+++ JS/index.js
@@ -24,11 +24,9 @@ function recipesCards() {
     //console.log(liste);
 
     recipes[i].ingredients.forEach((ing) => {
-      let test = new Set(recipes[i].ingredients);
-      console.log(test);
       let displayIng = `
       <li><span class="bold">${ing.ingredient}</span>: ${
-        ing.quantity ? ingredients.quantity : ""
+        ing.quantity ? ing.quantity : ""
       }
          ${ing.unit ? ing.unit : ""}</li>
       `;
@@ -44,18 +42,27 @@ function ingredientsInput() {
     "filtre-liste-ingredients"
   );
   const inputIng = document.getElementById("ingredients");
-  inputIng.addEventListener("focus", function () {
+  inputIng.addEventListener("click", function () {
     filtreListeIngredients.style.display = "grid";
     for (let i = 0; i < recipes.length; i++) {
       for (let j = 0; j < recipes[i].ingredients.length; j++) {
         let listeItems = `
-        <li>${recipes[i].ingredients[j].ingredient}</li>
+        <li onclick="createDiv(${recipes[i].ingredients[j].ingredient})">${recipes[i].ingredients[j].ingredient}</li>
         `;
         filtreListeIngredients.innerHTML += listeItems;
+        //listeItems.addEventListener("click", createDiv(listeItems));
       }
+      /*       let test = new Set(recipes[i].ingredients);
+      console.log(test);
+      test.forEach((ing) => {
+        let displayIng = `
+        <li>${ing.ingredient}</li>
+        `;
+        filtreListeIngredients.innerHTML += displayIng;
+      }); */
     }
   });
-  inputIng.addEventListener("blur", function () {
+  body.addEventListener("blur", function () {
     filtreListeIngredients.innerHTML = "";
     filtreListeIngredients.style.display = "none";
   });
@@ -68,7 +75,7 @@ function ingredientsAppareils() {
     "filtre-liste-appareils"
   );
   const inputAppareils = document.getElementById("appareils");
-  inputAppareils.addEventListener("focus", function () {
+  inputAppareils.addEventListener("click", function () {
     filtreListeAppareils.style.display = "block";
     for (let i = 0; i < recipes.length; i++) {
       for (let j = 0; j < recipes[i].ingredients.length; j++) {
@@ -92,7 +99,7 @@ function ingredientsUstensils() {
     "filtre-liste-ustensils"
   );
   const inputUstensils = document.getElementById("ustensils");
-  inputUstensils.addEventListener("focus", function () {
+  inputUstensils.addEventListener("click", function () {
     for (let i = 0; i < recipes.length; i++) {
       filtreListeUstensils.style.display = "block";
       for (let j = 0; j < recipes[i].ingredients.length; j++) {
@@ -110,3 +117,11 @@ function ingredientsUstensils() {
 }
 
 ingredientsUstensils();
+
+function createDiv(value) {
+  let containerArticle = document.getElementById("liste-findArticles");
+  //console.log(containerArticle);
+  const element = "<li>" + value + "</li>";
+  //  const element = `<li>value</li>`;
+  containerArticle.innerHTML += element;
+}
