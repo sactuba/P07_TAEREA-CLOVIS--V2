c8afcc47f4cac84c8e8e12393c1568bbcc2f8596
sactuba <taloamn85@gmail.com>
2022-05-06 13:00:31 +0200

    mise a jour

--- JS/cssFunction.js
+++ JS/cssFunction.js
@@ -1,11 +1,1 @@
-let ingredientsV2 = [];
-let applianceV2 = [];
-let ustensilsV2 = [];
 
-function getIngredientsV2() {
- while (i < recipes.length) {
-  console.log(recipies.name);
- }
-}
-
-getIngredientsV2();
--- JS/index2.js
+++ JS/index2.js
@@ -1,1 +1,32 @@
- 
+let ingredientsV2 = [];
+let applianceV2 = [];
+let ustensilsV2 = [];
+
+let ingredientsArrayV2 = [];
+let appareilsArrayV2 = [];
+let ustensilsArrayV2 = [];
+let findArticlesArrayV2 = [];
+
+function getIngredientsV2() {
+ let ing = recipes.map((recipe) => recipe.ingredients);
+ /*  let test = ing.reduce(function (prev, curr) {
+  return [...prev, ...curr.ingerdient];
+ }); */
+ console.log(ing);
+}
+getIngredientsV2();
+
+function getAppliancesV2() {
+ let appliances = recipes.map((recipe) => recipe.appliance);
+ appareilsArrayV2.push(appliances);
+ applianceV2 = Array.from(new Set(appareilsArrayV2));
+ console.log(applianceV2);
+}
+getAppliancesV2();
+
+function getUstensilsV2() {
+ let ustensils = recipes.map((recipe) => recipe.ustensils);
+ ustensilsArrayV2.push(ustensils);
+ console.log(ustensils);
+}
+getUstensilsV2();
